14674e5e4b5a7daa741d928758f314902322db9e
Goddii <otienogodwin23@gmail.com>
2026-03-20 22:26:12 +0300

    add functions to index.js

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
+function calculateTax(amount) {
+    if ((amount > 0) || (amount < 0) ){
+        const taxPercentage = (10/100)
+        let taxAmount = amount * taxPercentage
+        return taxAmount
+    
+        
+    } else {
+        return 0
+    }
+    
+    
+
+}
+
+function convertToUpperCase(text) {
+    let textIsUpper = text.toUpperCase()
+
+    return textIsUpper
+}
+
+function findMaximum(num1, num2) {
+    if (num1 > num2) {
+        return num1
+    } else {
+        return num2
+    }
+}
+
+function isPalindrome(word) {
+    const wordStr = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reverseWord = wordStr.split('').reverse().join('');
+
+    if (wordStr === reverseWord) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    const discountAmount = originalPrice * (discountPercentage / 100)
+
+    const totalAmount = originalPrice - discountAmount
+
+    return totalAmount
+
+}
 
 
 
@@ -5,3 +53,5 @@
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+
+
